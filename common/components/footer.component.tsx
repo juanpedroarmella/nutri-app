556129d78da29293376b0425e295aec155f3c3cf
Juan Pedro Armella <jparmella@changetheblock.com>
2025-02-12 05:24:12 -0300

    fix(app): small fix

--- common/components/footer.component.tsx
+++ common/components/footer.component.tsx
@@ -4,7 +4,7 @@ import { Instagram, Phone } from 'lucide-react'
 
 export function Footer() {
   return (
-    <footer className='w-full relative flex flex-col sm:flex-row items-center justify-between border-t text-center text-[10px] gap-4 py-4 px-6 text-muted-foreground'>
+    <footer className='w-full relative flex flex-col sm:flex-row items-center justify-between border-t text-center text-[10px] gap-4 py-4 px-6 text-muted-foreground mt-10 sm:mt-0'>
       <div className='flex items-center gap-4'>
         <p>
           © {new Date().getFullYear()} • Powered by{' '}
