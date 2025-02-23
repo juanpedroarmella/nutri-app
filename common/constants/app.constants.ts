7b3d498fae534ec79420fc85cd4e07a5094104d1
Juan Pedro Armella <jparmella@changetheblock.com>
2025-02-22 21:56:47 -0300

    fix(app): proyect name

--- common/constants/app.constants.ts
+++ common/constants/app.constants.ts
@@ -1,2 +1,2 @@
-export const APP_NAME = 'Nutri-Soft'
+export const APP_NAME = 'NutriSoft'
 export const APP_DESCRIPTION = 'Portal de nutrición profesional personalizada para el seguimiento de pacientes y planes nutricionales.'
