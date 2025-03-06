7aa81f3e506e355a5df5bfe3d2781da777f19506
Juan Pedro Armella <jparmella@changetheblock.com>
2025-03-06 17:06:21 -0300

    fix(email): security route

--- features/email/services/email.service.ts
+++ features/email/services/email.service.ts
@@ -2,15 +2,18 @@
 
 import { AuthRoutes } from '@/common/types/routes.types'
 import { EnvVariables } from '@/common/utils/env.utils'
+import { cookies } from 'next/headers'
 
 export async function sendWelcomeEmail(email: string, password: string) {
   try {
     const baseUrl = EnvVariables.nextPublicAppUrl
+    const cookie = (await cookies()).toString()
 
     const response = await fetch(`${baseUrl}/api/email/welcome`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        Cookie: cookie
       },
       body: JSON.stringify({
         userEmail: email,
@@ -20,7 +23,8 @@ export async function sendWelcomeEmail(email: string, password: string) {
     })
 
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
+      const error = await response.json()
+      throw new Error(`HTTP error! status: ${response.status} ${error.error}`)
     }
 
     return response.json()
