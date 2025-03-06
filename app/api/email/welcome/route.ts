3886c2a9a72e210a10f07b7a3a871e40a84cb139
Juan Pedro Armella <jparmella@changetheblock.com>
2025-03-06 17:38:13 -0300

    fix(email): security

--- app/api/email/welcome/route.ts
+++ app/api/email/welcome/route.ts
@@ -1,49 +1,12 @@
 import { APP_NAME } from '@/common/constants/app.constants'
+import { EnvVariables } from '@/common/utils/env.utils'
 import WelcomeEmail from '@/features/email/components/welcome-user.email'
 import { Resend } from 'resend'
-import { EnvVariables } from '@/common/utils/env.utils'
-import { createClient as createClientSupabase } from '@supabase/supabase-js'
 
 const resend = new Resend(EnvVariables.resendApiKey)
 
 export async function POST(request: Request) {
   try {
-    const authHeader = request.headers.get('Authorization')
-    
-    if (!authHeader?.startsWith('Bearer ')) {
-      return Response.json({ error: 'No autorizado' }, { status: 401 })
-    }
-
-    const token = authHeader.split(' ')[1]
-    
-    const supabase = createClientSupabase(
-      EnvVariables.supaBaseUrl!,
-      EnvVariables.supaBaseAnonKey!,
-      {
-        global: {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        }
-      }
-    )
-
-    const { data: { user }, error: userError } = await supabase.auth.getUser()
-
-    if (userError || !user) {
-      return Response.json({ error: 'No autorizado' }, { status: 401 })
-    }
-
-    const { data: userData, error: userDataError } = await supabase
-      .from('users')
-      .select('role')
-      .eq('id_auth', user.id)
-      .single()
-
-    if (userDataError || !userData || userData.role !== 'admin') {
-      return Response.json({ error: 'No tienes permisos para enviar correos' }, { status: 403 })
-    }
-
     const { userEmail, password, redirectUrl } = await request.json()
 
     const { data, error } = await resend.emails.send({
--- features/email/services/email.service.ts
+++ features/email/services/email.service.ts
@@ -2,22 +2,15 @@
 
 import { AuthRoutes } from '@/common/types/routes.types'
 import { EnvVariables } from '@/common/utils/env.utils'
-import { createClient } from '@/common/utils/supabase/server'
-import { cookies } from 'next/headers'
 
 export async function sendWelcomeEmail(email: string, password: string) {
   try {
-    const supabase = await createClient()
-    const { data: { session } } = await supabase.auth.getSession()
-    const cookieStore = await cookies()
     const baseUrl = EnvVariables.nextPublicAppUrl
 
     const response = await fetch(`${baseUrl}/api/email/welcome`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        Cookie: cookieStore.toString(),
-        Authorization: `Bearer ${session?.access_token}`
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         userEmail: email,
