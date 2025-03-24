4e313cee03565e50c80fa89d25187f66350b4711
Juan Pedro Armella <juanpedroarmella@gmail.com>
2025-03-24 17:49:49 -0300

    fix(component): dropdown

--- common/components/main-nav.component.tsx
+++ common/components/main-nav.component.tsx
@@ -8,13 +8,13 @@ import { UserCardSkeleton } from './user-card-skeleton.component'
 
 export function MainNav() {
   return (
-    <nav className='w-full fixed top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center border-b border-b-foreground/10 h-20'>
-      <div className='w-full flex justify-between items-center p-3 py-0 px-16 text-sm max-w-7xl mx-auto gap-6'>
+    <nav className='w-full fixed top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center border-b border-b-foreground/10 h-16'>
+      <div className='w-full flex justify-between items-center py-2 sm:px-16 px-2 text-sm max-w-7xl mx-auto gap-6'>
         <Link
           href={'/'}
-          className='h-[75%] w-40 relative'
+          className='h-full w-[132px] relative'
         >
-          <Image src='/logo.svg' alt='Main Logo' fill className='rounded-2xl'/>
+          <Image src='/logo.svg' alt='Main Logo' fill className='rounded-tr-[12px]' objectFit='contain'/>
         </Link>
         {!hasEnvVars ? (
           <EnvVarWarning />
