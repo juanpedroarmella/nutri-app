28a524fc54ecc6b6cc83796086bec2942e2706e0
Juan Pedro Armella <juanpedroarmella@gmail.com>
2025-08-05 23:31:13 -0300

    fix(nav): adjust logo styling and navbar padding

--- common/components/main-nav.component.tsx
+++ common/components/main-nav.component.tsx
@@ -9,12 +9,12 @@ import { UserCardSkeleton } from './user-card-skeleton.component'
 export function MainNav() {
   return (
     <nav className='w-full fixed top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center border-b border-b-foreground/10 h-16'>
-      <div className='w-full flex justify-between items-center py-2 sm:px-16 px-2 text-sm max-w-7xl mx-auto gap-6'>
+      <div className='w-full flex justify-between items-center py-1 sm:px-16 px-2 text-sm max-w-7xl mx-auto gap-6'>
         <Link
           href={'/'}
           className='h-full w-[132px] relative'
         >
-          <Image src='/logo.svg' alt='Main Logo' fill className='rounded-tr-[12px]' objectFit='contain'/>
+          <Image src='/logo.svg' alt='Main Logo' fill className='rounded-br-[24px]' objectFit='contain'/>
         </Link>
         {!hasEnvVars ? (
           <EnvVarWarning />
